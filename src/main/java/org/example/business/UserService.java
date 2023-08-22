cf246abc5cc7a28dc5c2f48499085d25af15046c
gholami.f84@gmail.com <09187807913>
2023-08-22 08:45:07 +0330

    security

--- src/main/java/org/example/business/UserService.java
+++ src/main/java/org/example/business/UserService.java
@@ -4,17 +4,23 @@ import lombok.AllArgsConstructor;
 import org.example.exceptions.NotFoundException;
 import org.example.model.UserRequest;
 import org.example.model.UserResponse;
+import org.example.repo.RoleEntity;
+import org.example.repo.RoleRepository;
 import org.example.repo.UserEntity;
 import org.example.repo.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -24,6 +30,9 @@ public class UserService {
         entity.setActive(true);
         entity.setDeleted(false);
         BeanUtils.copyProperties(request, entity);
+        Optional<RoleEntity> roleEntity = roleRepository.findByName("Admin");
+        if (roleEntity.isPresent())
+            entity.getRoles().add(roleEntity.get());
         entity = userRepository.save(entity);
         UserResponse response = new UserResponse();
         BeanUtils.copyProperties(entity, response);
--- src/main/java/org/example/repo/RoleRepository.java
+++ src/main/java/org/example/repo/RoleRepository.java
@@ -2,9 +2,14 @@ package org.example.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.expression.spel.ast.OpAnd;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long>, JpaSpecificationExecutor<RoleEntity> {
+
+    Optional<RoleEntity> findByName(String name);
 
 }
